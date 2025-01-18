b293f83a4bd5b614c79a258314534d2b50b93aba
DavidPerea <d.perearomero@proton.me>
2025-01-18 17:19:07 +0100

    Added Query, Headers and Ip decorators.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Ip,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-    @Get()
-    public getUsers() {
-        return 'You just send a GET request to "/users" endpoint';
-    }
-    @Post()
-    public createUsers() {
-        return 'You just created an user via POST on "/users" endpoint';
-    }
+  @Get('/:id?')
+  public getUsers(@Param() params: any, @Query() query: any) {
+    console.log(params);
+    console.log(query);
+    return 'You just send a GET request to "/users" endpoint';
+  }
+  @Post()
+  public createUsers(
+    @Body() param: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(param);
+    console.log(headers);
+    console.log(ip);
+    return 'You just created an user via POST on "/users" endpoint';
+  }
 }

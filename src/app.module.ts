53399459111cb0ad316b01b016e18be71e620895
DavidPerea96 <daviid_perea_@hotmail.com>
2025-04-12 12:41:47 +0200

    Created User Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,15 @@ import { UsersModule } from './users/users.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [UsersModule, PostModule, AuthModule, TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-      type: 'postgres',
-      entities: [],
+    imports: [],
+    
+    useFactory: () => ({
+      type: "postgres",
+      entities: [User],
       synchronize: true,
       port: 5432,
       username: 'postgres',

f1335e86c3bcf64633f0e8c2a69f2def65ce008b
DavidPerea96 <daviid_perea_@hotmail.com>
2025-04-12 12:16:48 +0200

    Added connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostModule, AuthModule],
+  imports: [UsersModule, PostModule, AuthModule, TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+      type: 'postgres',
+      entities: [],
+      synchronize: true,
+      port: 5432,
+      username: 'postgres',
+      password: 'El-nevado96@',
+      host: 'localhost',
+      database: 'nestjs-intro',
+    }),
+  })],
   controllers: [AppController],
   providers: [AppService],
 })

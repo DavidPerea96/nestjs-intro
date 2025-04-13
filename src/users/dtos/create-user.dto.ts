117cc42cfd64045f77b3066892c7727e15bb6adb
DavidPerea96 <daviid_perea_@hotmail.com>
2025-04-13 11:59:53 +0200

    Minor update

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -12,26 +12,28 @@ export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
-  @MaxLength(40)
+  @MaxLength(96)
   firstName: string;
 
   @IsString()
   @IsOptional()
   @MinLength(3)
-  @MaxLength(40)
+  @MaxLength(96)
   lastName?: string;
 
   @IsEmail()
   @IsNotEmpty()
-  @MaxLength(50)
+  @MinLength(8)
+  @MaxLength(96)
   email: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
+  @MaxLength(96)
   @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
     message:
-      'Minimum 8 characters, 1 special character, 1 letter and 1 number.',
+      'Minimum 8 characters, 1 special character, 1 letter and 1 number. Maximum 96 characters.',
   })
   password: string;
 }
